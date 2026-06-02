fa9f085c88d051dc446eba295ea0168ff75eb50c
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-06-02 14:51:18 +0200

    Lösungen

---
+++ 02_konsole/05_if-anweisung/01_beispiel.py
@@ -0,0 +1,7 @@
+vorname = input("Wie heisst du?")
+print(f"Hallo {vorname}. Schön dich zu sehen.")
+
+if vorname == "Alice":
+    print(f"{vorname} ist ein schöner Name.")
+
+print("Tschüss") 
---
+++ 02_konsole/05_if-anweisung/04_wochentag.py
@@ -0,0 +1,4 @@
+import random as rd
+
+wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+wochentag = rd.choice(wochentage)
